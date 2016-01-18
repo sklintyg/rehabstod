56a487d7bcdd0f6d04ea91f4293fb8d1969bad68
Magnus Ekstrand <magnus@ekstrand.net>
2016-01-18 10:21:38 +0100

    DEVENV: Removed persistance and FitNesse folder since we're not gonna use it

--- specifications/src/main/groovy/se/inera/privatlakarportal/page/RegisterPage.groovy
+++
@@ -1,6 +0,0 @@
-package se.inera.privatlakarportal.page
-
-class RegisterPage extends AbstractPage {
-    static url = System.getProperty("privatlakarportal.baseUrl")
-    static at = { title == "Privatläkarportalen" }
-}
