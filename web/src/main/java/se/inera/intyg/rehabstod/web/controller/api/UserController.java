55116d0ee85f6b00b68ac7ff68c6ad5697a0c794
Erik Lupander <erik.lupander@callistaenterprise.se>
2016-01-19 13:17:35 +0100

    REHAB-52: Fixed rudimentary user service

--- web/src/main/java/se/inera/intyg/rehabstod/web/controller/api/UserController.java
+++ web/src/main/java/se/inera/intyg/rehabstod/web/controller/api/UserController.java
@@ -1,6 +1,7 @@
 package se.inera.intyg.rehabstod.web.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import se.inera.intyg.rehabstod.auth.RehabstodUser;
 import se.inera.intyg.rehabstod.service.model.User;
@@ -13,13 +14,15 @@ import se.inera.intyg.rehabstod.web.controller.api.dto.GetUserResponse;
 @RequestMapping("/api/user")
 public class UserController {
 
-    //@Autowired
-    //private UserService userService;
-    //TODO: Add rehabstodUser properties such as careunits etc
-
     @RequestMapping(value = "")
     public GetUserResponse getUser() {
-        return new GetUserResponse(new User(new RehabstodUser("111111", "kalle banan"), false));
+
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            return null;
+        }
+        RehabstodUser user = (RehabstodUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return new GetUserResponse(new User(user, false));
     }
 
 }
