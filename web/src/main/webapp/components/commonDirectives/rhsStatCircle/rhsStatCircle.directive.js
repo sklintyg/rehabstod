68f01bf6d9cc2136f1c9461d69e93a4d059af4e2
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-01-21 09:05:12 +0100

    REHAB-50: Lagt till initial statistics direktiv och template för statistik-bubblan

---
+++ web/src/main/webapp/components/commonDirectives/rhsStatCircle/rhsStatCircle.directive.js
@@ -0,0 +1,21 @@
+/**
+ * Enable tooltips for other components than wcFields
+ */
+angular.module('rehabstodApp').directive('rhsStatCircle',
+    ['messageService',
+        function(messageService) {
+            'use strict';
+
+            return {
+                restrict: 'E',
+                transclude: true,
+                scope: {
+                    size: '@',
+                    backgroundColor: '@'
+                },
+                controller: function($scope) {
+
+                },
+                templateUrl: 'components/commonDirectives/rhsStatCircle/rhsStatCircle.directive.html'
+            };
+        }]);
