7daa155d5f98f3c80745ef0fba47f0bc8da85376
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-01-29 10:47:43 +0100

    REHAB-59: Visar antal post i tabellen

---
+++ web/src/main/webapp/components/commonDirectives/rhsTableNumberOfRows/rhsTableNumberOfRows.directive.spec.js
@@ -0,0 +1,42 @@
+describe('Directive: RhsTableNumberOfRows', function () {
+    'use strict';
+
+    // load the controller's module
+    beforeEach(module('rehabstodApp'));
+
+    var $compile;
+    var $scope;
+
+    // Store references to $rootScope and $compile
+    // so they are available to all tests in this describe block
+    beforeEach(inject(function(_$compile_, $rootScope){
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $compile = _$compile_;
+        $scope = $rootScope.$new();
+    }));
+
+    xit('ensures callFoo does whatever it is supposed to', function() {
+        // Arrange
+
+        var tableCtrl = {
+            getFilteredCollection: function() { return [1,2,3]; }
+        };
+
+        spyOn(tableCtrl, 'getFilteredCollection').and.callThrough();
+
+        var element = angular.element('<rhs-table-number-of-rows></rhs-table-number-of-rows>');
+        element.data('$stTableController', tableCtrl);
+
+        var rowElement = $compile(element)($scope);
+
+        var rowsScope = rowElement.scope();
+
+        // Act
+        $scope.$digest();
+
+        // Assert
+
+        expect(rowsScope.rows).toEqual(3);
+        expect(tableCtrl.getFilteredCollection).toHaveBeenCalled();
+    });
+});
