3999a9c62d4bcba3dba6d6631731285e27db49ff
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-02-01 11:17:34 +0100

    REHAB-11: Lagt in tester för hämtning av sjukfall

--- web/src/main/webapp/app/sjukfall/sjukfall.page.controller.spec.js
+++ web/src/main/webapp/app/sjukfall/sjukfall.page.controller.spec.js
@@ -1,16 +1,79 @@
 describe('Controller: SjukfallPageCtrl', function () {
-  'use strict';
+    'use strict';
 
-  // load the controller's module
-  beforeEach(module('rehabstodApp'));
+    // load the controller's module
+    beforeEach(module('rehabstodApp'));
 
-  var scope;
+    var scope;
+    var SjukfallModel;
+    var SjukfallService;
+    var sjukFallData = [{name: "Hej"}];
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    scope = $rootScope.$new();
-    $controller('SjukfallPageCtrl', {
-      $scope: scope
+    describe('Load succeeded ', function () {
+        // Initialize the controller and a mock scope
+        beforeEach(inject(function($controller, $rootScope, $q, _SjukfallModel_, _SjukfallService_) {
+            scope = $rootScope.$new();
+
+            SjukfallService = _SjukfallService_;
+            SjukfallModel = _SjukfallModel_;
+
+            spyOn(SjukfallService, "loadSjukfall").and.callFake(function() {
+                var promise = $q.defer();
+
+                promise.resolve(sjukFallData);
+
+                return promise.promise;
+            });
+
+
+            $controller('SjukfallPageCtrl', {
+                $scope: scope,
+                SjukfallModel: SjukfallModel,
+                SjukfallService: SjukfallService
+            });
+
+            scope.$digest();
+        }));
+
+
+        it('Test load of Sjukfall', function() {
+            expect(SjukfallModel.get()).toEqual(sjukFallData);
+        });
+
     });
-  }));
+
+    describe('Load failed ', function () {
+        // Initialize the controller and a mock scope
+        beforeEach(inject(function($controller, $rootScope, $q, _SjukfallModel_, _SjukfallService_) {
+            scope = $rootScope.$new();
+
+            SjukfallService = _SjukfallService_;
+            SjukfallModel = _SjukfallModel_;
+
+            SjukfallModel.set(sjukFallData);
+
+            spyOn(SjukfallService, "loadSjukfall").and.callFake(function() {
+                var promise = $q.defer();
+
+                promise.reject();
+
+                return promise.promise;
+            });
+
+
+            $controller('SjukfallPageCtrl', {
+                $scope: scope,
+                SjukfallModel: SjukfallModel,
+                SjukfallService: SjukfallService
+            });
+
+            scope.$digest();
+        }));
+
+
+        it('Test load of Sjukfall', function() {
+            expect(SjukfallModel.get()).toEqual([]);
+        });
+
+    });
 });
