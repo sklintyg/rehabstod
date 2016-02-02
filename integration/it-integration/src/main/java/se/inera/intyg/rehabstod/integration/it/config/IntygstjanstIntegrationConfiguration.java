89b7824ce6e4f2ff941e1377c7f9a87e345e8b6e
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-02-02 08:43:14 +0100

    DEVENV: Fixade PMD errors

--- integration/it-integration/src/main/java/se/inera/intyg/rehabstod/integration/it/config/IntygstjanstIntegrationConfiguration.java
+++ integration/it-integration/src/main/java/se/inera/intyg/rehabstod/integration/it/config/IntygstjanstIntegrationConfiguration.java
@@ -18,44 +18,34 @@
  */
 package se.inera.intyg.rehabstod.integration.it.config;
 
-import org.apache.cxf.Bus;
-
-import org.apache.cxf.jaxws.EndpointImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-import se.inera.intyg.rehabstod.integration.it.stub.SjukfallIntygStub;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Configuration
 @ComponentScan({
         "se.inera.intyg.rehabstod.integration.it.client",
-        "se.inera.intyg.rehabstod.integration.it.service"})
+        "se.inera.intyg.rehabstod.integration.it.service" })
 @ImportResource("classpath:it-services.xml")
 public class IntygstjanstIntegrationConfiguration {
-//
-//    @Autowired
-//    private ApplicationContext applicationContext;
-//
-//    @Autowired
-//    private SjukfallIntygStub sjukfallIntygStub;
-//
-//    @Bean
-//    public ObjectMapper objectMapper() {
-//        return new ObjectMapper();
-//    }
-//
-//    @Bean
-//    public EndpointImpl intygstjanstResponder() {
-//        Bus bus = (Bus) applicationContext.getBean(Bus.DEFAULT_BUS_ID);
-//        Object implementor = sjukfallIntygStub;
-//        EndpointImpl endpoint = new EndpointImpl(bus, implementor);
-//        endpoint.publish("/get-active-sickleaves-for-careunit/v1.0");
-//        return endpoint;
-//    }
+    //
+    // @Autowired
+    // private ApplicationContext applicationContext;
+    //
+    // @Autowired
+    // private SjukfallIntygStub sjukfallIntygStub;
+    //
+    // @Bean
+    // public ObjectMapper objectMapper() {
+    // return new ObjectMapper();
+    // }
+    //
+    // @Bean
+    // public EndpointImpl intygstjanstResponder() {
+    // Bus bus = (Bus) applicationContext.getBean(Bus.DEFAULT_BUS_ID);
+    // Object implementor = sjukfallIntygStub;
+    // EndpointImpl endpoint = new EndpointImpl(bus, implementor);
+    // endpoint.publish("/get-active-sickleaves-for-careunit/v1.0");
+    // return endpoint;
+    // }
 }
--- integration/it-integration/src/main/java/se/inera/intyg/rehabstod/integration/it/stub/IntygstjanstIntegrationStubConfiguration.java
+++ integration/it-integration/src/main/java/se/inera/intyg/rehabstod/integration/it/stub/IntygstjanstIntegrationStubConfiguration.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2016 Inera AB (http://www.inera.se)
+ *
+ * This file is part of rehabstod (https://github.com/sklintyg/rehabstod).
+ *
+ * rehabstod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rehabstod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.inera.intyg.rehabstod.integration.it.stub;
 
 import org.apache.cxf.Bus;
@@ -7,14 +25,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Profile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Configuration
-@ComponentScan({"se.inera.intyg.rehabstod.integration.it.stub"})
-@Profile({"dev", "rhs-it-stub"})
+@ComponentScan({ "se.inera.intyg.rehabstod.integration.it.stub" })
+@Profile({ "dev", "rhs-it-stub" })
 public class IntygstjanstIntegrationStubConfiguration {
 
     @Autowired
