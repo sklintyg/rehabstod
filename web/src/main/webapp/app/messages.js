218e897069a811c0fcb44dbedade7088fdd1361f
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-02-08 15:17:02 +0100

    REHAB-4: Snyggat till pagineringen

--- web/src/main/webapp/app/messages.js
+++ web/src/main/webapp/app/messages.js
@@ -23,7 +23,25 @@ var ppMessages = {
         // Header
 
         // Start
-        'label.start.header': 'Rehabstöd startsida'
+        'label.start.header': 'Rehabstöd startsida',
+
+
+        // Table
+        'label.table.column.pnr': 'Personnummer',
+        'label.table.column.namn': 'Namn',
+        'label.table.column.diagnos': 'Diagnos',
+        'label.table.column.diagnos.help': 'Huvuddiagnosen i det intyg som är giltigt just nu. För muspekaren över koden för att se vilken diagnos den motsvarar.',
+        'label.table.column.startvg': 'Startdatum vårdgivare',
+        'label.table.column.startve': 'Startdatum vårdenhet',
+        'label.table.column.slut': 'Slutdatum',
+        'label.table.column.slut.help': 'Slutdatum för det intyg som är giltigt just nu, dvs. det datum då sjukskrivningen beräknas avslutas om den inte förlängs.',
+        'label.table.column.grad': 'Grad',
+        'label.table.column.grad.help': 'Sjukskrivningsgraden i det intyg som är giltigt just nu. Om det innehåller flera grader anges de ordnade i tidsföljd med den just nu gällande graden fetstilsmarkerad.',
+        'label.table.column.lakare': 'Läkare',
+        'label.table.column.lakare.help': 'Läkaren som utfärdat det intyg som är giltigt just nu.',
+        'label.table.column.langd': 'Sjukskrivningslängd',
+        'label.table.column.langd.help': 'Sjukskrivningens totala längd i dagar, från ”Startdatum vårdgivare” till ”Slutdatum”. Dagar då patienten inte haft något giltigt intyg räknas inte.'
+
 
     },
     'en': {
