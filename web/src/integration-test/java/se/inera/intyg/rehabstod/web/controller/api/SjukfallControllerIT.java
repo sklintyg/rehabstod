b989fa84d8f4ba37248e9896a925620ed5957307
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-02-09 10:16:15 +0100

    BUILDENV: Skapat rest-assured test för sjukfall

--- web/src/integration-test/java/se/inera/intyg/rehabstod/web/controller/api/SjukfallControllerIT.java
+++ web/src/integration-test/java/se/inera/intyg/rehabstod/web/controller/api/SjukfallControllerIT.java
@@ -19,8 +19,10 @@
 package se.inera.intyg.rehabstod.web.controller.api;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 import org.junit.Test;
 import se.inera.intyg.rehabstod.web.BaseRestIntegrationTest;
+import se.inera.intyg.rehabstod.web.controller.api.dto.GetSjukfallRequest;
 
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
@@ -50,5 +52,17 @@ public class SjukfallControllerIT extends BaseRestIntegrationTest {
 
         given().expect().statusCode(OK).when().get(API_ENDPOINT + "/summary").then().
                 body(matchesJsonSchemaInClasspath("jsonschema/rhs-sjukfallsummary-response-schema.json"));
+    }
+
+    @Test
+    public void testGetSjukfall() {
+
+        RestAssured.sessionId = getAuthSession(DEFAULT_LAKARE);
+
+        GetSjukfallRequest request = new GetSjukfallRequest();
+        request.setMaxIntygsGlapp(0);
+
+        given().contentType(ContentType.JSON).and().body(request).expect().statusCode(OK).when().post(API_ENDPOINT).then().
+                body(matchesJsonSchemaInClasspath("jsonschema/rhs-sjukfall-response-schema.json"));
     }
 }
