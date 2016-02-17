a6c552063c44b5880907b9c3652f32098d6b7bbc
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-02-17 09:33:53 +0100

    REHAB-78: Refactorerat hämtningen av sjukfall

--- web/src/main/webapp/app/sjukfall/result/result.page.controller.js
+++ web/src/main/webapp/app/sjukfall/result/result.page.controller.js
@@ -1,19 +1,7 @@
 angular.module('rehabstodApp')
     .controller('SjukfallResultPageCtrl',
-        function($scope, $state, $log, SjukfallModel, SjukfallService) {
+        function(SjukfallService) {
             'use strict';
 
-            $log.debug('SjukfallResultPageCtrl init');
-
-            SjukfallModel.reset();
-            SjukfallService.loadSjukfall().then(function(successData) {
-                SjukfallModel.set(successData);
-            }, function(errorData) {
-                $log.debug('Failed to get sjukfall.');
-                $log.debug(errorData);
-
-                SjukfallModel.reset();
-            });
-
-
+            SjukfallService.loadSjukfall();
         });
--- web/src/main/webapp/app/sjukfall/result/sjukfall.service.js
+++ web/src/main/webapp/app/sjukfall/result/sjukfall.service.js
@@ -1,76 +1,23 @@
 angular.module('rehabstodApp').factory('SjukfallService', [
-    '$log', '$q', 'SjukfallProxy',
-    function($log, $q, SjukfallProxy) {
+    '$log','SjukfallProxy', 'SjukfallModel',
+    function($log, SjukfallProxy, SjukfallModel) {
         'use strict';
 
-        /*function _loadSjukfallTemp() {
-            var
-                nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa'],
-                familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez'],
-                diagnosName = ['F 3.21', 'M', 'F 3.22', '-'],
-                lakareName = ['Mästerkatten', 'Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez'],
-                grader = [100, 50, 25];
-
-            function createRandomItem() {
-                var
-                    firstName = nameList[Math.floor(Math.random() * 5)],
-                    lastName = familyName[Math.floor(Math.random() * 5)],
-                    lakare = lakareName[Math.floor(Math.random() * 6)],
-                    diagnos = diagnosName[Math.floor(Math.random() * 4)],
-                    langd = 1 + Math.floor(Math.random() * 400),
-                    dag = Math.floor(Math.random() * 30) + 1,
-                    grad = grader[Math.floor(Math.random() * 3)];
-
-                if (dag < 10) {
-                    dag = '0' + dag;
-                }
-
-                return {
-                    patient :{
-                        id: '19700123-9297',
-                        alder: 12,
-                        kon: 'f',
-                        namn: firstName + ' ' + lastName
-                    },
-                    diagnos : {
-                        original: diagnos,
-                        grupp: null,
-                        kod: null
-                    },
-                    startVG: '2015-09-' + dag,
-                    startVE: '2015-10-' + dag,
-                    slut: '2015-12-15',
-                    dagar: langd,
-                    intyg: Math.floor(Math.random() * 5) + 1,
-                    grader: grader,
-                    aktivGrad: grad,
-                    lakare: lakare
-                };
-            }
-
-            var collection = [];
-            for (var j = 0; j < 2000; j++) {
-                collection.push(createRandomItem());
-            }
-
-            return collection;
-        }*/
-
         function _loadSjukfall() {
+            SjukfallModel.reset();
+
             var query = {
                 maxIntygsGlapp: 0
             };
 
-            return SjukfallProxy.get(query);
+            return SjukfallProxy.get(query).then(function(successData) {
+                SjukfallModel.set(successData);
+            }, function(errorData) {
+                $log.debug('Failed to get sjukfall.');
+                $log.debug(errorData);
 
-
-            /*
-
-            var promise = $q.defer();
-
-            promise.resolve(_loadSjukfallTemp());
-
-            return promise.promise;*/
+                SjukfallModel.reset();
+            });
         }
 
         return {
