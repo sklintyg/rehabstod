0cee226ad63039c634e90fc6d745eb93ba1e3712
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-02-18 11:26:35 +0100

    REHAB-78: Lagt in texter i message.js och lagt till skugga på titlen när man skrollar

--- web/src/main/webapp/app/messages.js
+++ web/src/main/webapp/app/messages.js
@@ -40,12 +40,12 @@ var ppMessages = {
         'label.sjukfall.start.lakare.info.body' : 'Som läkare har du rätt att ta del av sjukskrivningar på alla enheter du har medarbetaruppdrag för. Dock är det viktigt att bara..... <br>' +
                                                     'Gå vidare genom att välja vilken information du vill ta del av. ',
 
-        'label.sjukfall.start.lakare.mina.header' : 'Endast de sjulfall där jag är nuvarande läkare',
-        'label.sjukfall.start.lakare.mina.body' : 'Som läkare kan du välja att endast ta del av de sjukskrivningar där du själv är nuvarande läkare.',
+        'label.sjukfall.start.lakare.mina.header' : 'Endast de sjukfall där jag utfärdat det aktiva intyget',
+        'label.sjukfall.start.lakare.mina.body' : 'Som läkare kan du välja att endast ta del av de sjukfall där du själv utfärdat det aktiva intyget',
         'label.sjukfall.start.lakare.mina.button' : 'Visa mina sjukfall',
 
         'label.sjukfall.start.lakare.alla.header' : 'Samtliga pågående fall på enheten',
-        'label.sjukfall.start.lakare.alla.body' : 'Välj dett alternativ om du har behov av att även se sjukfall där nuvarande läkare är andra läkare på vårdeneheten. För att ta del av dess skall du ha ett behov av det.',
+        'label.sjukfall.start.lakare.alla.body' : 'Välj detta alternativ om du har behov av att även se sjukfall där nuvarande läkare är andra läkare på vårdenheten. För att ta del av dess skall du ha ett behov av det.',
         'label.sjukfall.start.lakare.alla.button' : 'Visa alla sjukfall',
 
         // Sjukfall Stat
@@ -84,6 +84,9 @@ var ppMessages = {
         'label.table.column.lakare.help': 'Läkaren som utfärdat det intyg som är giltigt just nu.',
         'label.table.column.langd': 'Sjukskrivningslängd',
         'label.table.column.langd.help': 'Sjukskrivningens totala längd i dagar, från ”Startdatum vårdenhet” till ”Slutdatum”. Dagar då patienten inte haft något giltigt intyg räknas inte.',
+
+        'label.table.number.of.rows' : 'Antal poster:',
+
 
         // Filter
         'label.filter.diagnos' : 'Välj nuvarande diagnos',
