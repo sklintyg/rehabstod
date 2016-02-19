342ede503ebf4e842345e04d4882b2fa949e8e7b
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-02-19 09:03:03 +0100

    REHAB-86: Bara ladda sjukfall en gång i taget

--- web/src/main/webapp/components/services/sjukfall.service.js
+++ web/src/main/webapp/components/services/sjukfall.service.js
@@ -3,11 +3,18 @@ angular.module('rehabstodApp').factory('SjukfallService', [
     function($log, SjukfallProxy, SjukfallModel, SjukfallFilterViewState) {
         'use strict';
 
+        var loading = false;
+
         function _loadSjukfall(force) {
+
+            if (loading) {
+                return;
+            }
 
             var empty = SjukfallModel.get().length === 0;
 
             if (force || empty) {
+                loading = true;
                 SjukfallFilterViewState.reset();
                 SjukfallModel.reset();
 
@@ -17,11 +24,13 @@ angular.module('rehabstodApp').factory('SjukfallService', [
 
                 return SjukfallProxy.get(query).then(function(successData) {
                     SjukfallModel.set(successData);
+                    loading = false;
                 }, function(errorData) {
                     $log.debug('Failed to get sjukfall.');
                     $log.debug(errorData);
 
                     SjukfallModel.reset();
+                    loading = false;
                 });
             }
         }
