753347860de1152d5219cda4a8da19fcd224b9e2
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-02-19 16:05:12 +0100

    REHAB-82: Fixat till brytning av kolumnnamn när de inte får plats.

--- web/src/main/webapp/app/messages.js
+++ web/src/main/webapp/app/messages.js
@@ -69,7 +69,7 @@ var ppMessages = {
         'label.gender.undefined': '-',
 
         // Table
-        'label.table.column.pnr': 'Personnummer',
+        'label.table.column.pnr': 'Person&shy;nummer',
         'label.table.column.namn': 'Namn',
         'label.table.column.kon': 'Kön',
         'label.table.column.diagnos': 'Nuvarande diagnos',
@@ -78,7 +78,7 @@ var ppMessages = {
         'label.table.column.startve.help': 'Datum då sjukskrivningen började på ${enhet}. Alla intyg för samma patient som följer på varandra med max ${glapp} dagars uppehåll räknas till samma sjukskrivning. Max antal dagars uppehåll mellan intyg kan ställas in i filter.',
         'label.table.column.slut': 'Slutdatum',
         'label.table.column.slut.help': 'Slutdatum för det intyg som är giltigt just nu, dvs. det datum då sjukskrivningen beräknas avslutas om den inte förlängs.',
-        'label.table.column.grad': 'Nuvarande grad',
+        'label.table.column.grad': 'Sjukskrivnings&shy;grad',
         'label.table.column.grad.help': 'Sjukskrivningsgraden i det intyg som är giltigt just nu. Om det innehåller flera grader anges de ordnade i tidsföljd med den just nu gällande graden fetstilsmarkerad.',
         'label.table.column.lakare': 'Nuvarande läkare',
         'label.table.column.lakare.help': 'Läkaren som utfärdat det intyg som är giltigt just nu.',
