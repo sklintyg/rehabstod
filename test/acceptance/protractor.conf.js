57fff3bf54ac15395e96f4c4e4d9d6a41df2ae9c
MartinJanota <martin.janota@nordicmedtest.se>
2016-02-29 16:00:13 +0100

    ACCENV: modifiering av fonktionslokationen.

--- test/acceptance/protractor.conf.js
+++ test/acceptance/protractor.conf.js
@@ -76,15 +76,17 @@ exports.config = {
         });
 
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-    },
-    formatLocalDate: function formatLocalDate() {
-        var now = new Date(),
-            tzo = -now.getTimezoneOffset(),
-            dif = tzo >= 0 ? '+' : '-',
-            pad = function(num) {
-                var norm = Math.abs(Math.floor(num));
-                return (norm < 10 ? '0' : '') + norm;
-            };
-        return now.getFullYear() + '-' + pad(now.getMonth() + 1) + '-' + pad(now.getDate()) + 'T' + pad(now.getHours()) + ':' + pad(now.getMinutes()) + ':' + pad(now.getSeconds()) + dif + pad(tzo / 60) + ':' + pad(tzo % 60);
     }
+
 };
+
+function formatLocalDate() {
+    var now = new Date(),
+        tzo = -now.getTimezoneOffset(),
+        dif = tzo >= 0 ? '+' : '-',
+        pad = function(num) {
+            var norm = Math.abs(Math.floor(num));
+            return (norm < 10 ? '0' : '') + norm;
+        };
+    return now.getFullYear() + '-' + pad(now.getMonth() + 1) + '-' + pad(now.getDate()) + 'T' + pad(now.getHours()) + ':' + pad(now.getMinutes()) + ':' + pad(now.getSeconds()) + dif + pad(tzo / 60) + ':' + pad(tzo % 60);
+}
