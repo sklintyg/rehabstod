f011b491f6288f404e8d73535e2148a03c1815a2
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-03-01 12:45:37 +0100

    REHAB-94: Testar att byta enhet

---
+++ test/dev/spec/bytaEnhet.spec.js
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2016 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*globals pages */
+/*globals describe,it,helpers */
+'use strict';
+
+var navigationHelper = rhsTestTools.helpers.navigation;
+var specHelper = rhsTestTools.helpers.spec;
+var startPage = rhsTestTools.pages.startPage;
+var aboutPage = rhsTestTools.pages.aboutPage;
+var sjukfallPage = rhsTestTools.pages.sjukfallPage;
+
+describe('Logga in som Lena Karlsson', function() {
+
+
+
+    beforeEach(function() {
+        browser.ignoreSynchronization = false;
+        specHelper.login('IFV1239877878-104N_IFV1239877878-1045');
+
+    });
+
+    it('with user open and close select care unit', function() {
+        startPage.clickBytVardenhet();
+
+        expect(startPage.closeBytVardenhet.isPresent()).toBeTruthy();
+
+        startPage.clickCloseBytVardenhet();
+    });
+
+    it(' byta enhet på startsidan', function() {
+        bytEnhet();
+    });
+
+    it(' byt enhet på om sidan', function() {
+        navigationHelper.goToAbout();
+
+        bytEnhet();
+
+        expect(aboutPage.isAt()).toBeTruthy();
+    });
+
+    it(' byt enhet på sjukfalls sidan', function() {
+        startPage.clickFullUnit();
+        expect(sjukfallPage.isAt()).toBeTruthy();
+
+        bytEnhet();
+
+        expect(sjukfallPage.isAt()).toBeTruthy();
+    });
+
+
+    function bytEnhet() {
+        var today = new Date().toISOString().split('T')[0];
+
+        expect(element(by.id('location')).getText()).toBe(today + ' - WebCert-Vårdgivare2 - WebCert-Enhet2');
+
+        startPage.clickBytVardenhet();
+        element(by.id('select-active-unit-IFV1239877878-104D-modal')).click();
+
+        expect(element(by.id('location')).getText()).toBe(today + ' - WebCert-Vårdgivare2 - WebCert-Enhet3');
+    }
+
+
+    afterEach(function() {
+        specHelper.logout();
+    })
+});
