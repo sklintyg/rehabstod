6556b2abed8fa8e561888e75ba77288a0f9e47bf
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-03-08 16:04:06 +0100

    REHAB-110: Tagit bort tooltip på slider

--- web/src/main/webapp/components/commonDirectives/rhsSlider/rhsSlider.directive.js
+++ web/src/main/webapp/components/commonDirectives/rhsSlider/rhsSlider.directive.js
@@ -40,37 +40,6 @@ angular.module('rehabstodApp').directive('rhsSlider',
                     $scope.step = 1;
                     $scope.ticksSnapBounds = 1;
 
-                    $scope.formatterFn =  function(value) {
-                        var text;
-
-                        if ($scope.range) {
-                            if (value === $scope.sliderModel[1]) {
-                                text = 'Till';
-                            } else {
-                                text = 'Från';
-                            }
-
-                            text += '\n';
-
-                            if (value === $scope.max) {
-                                text += '365+';
-                            } else {
-                                text += value;
-                            }
-                        }
-                        else {
-                            text = value;
-                        }
-
-
-                        if (value === 1) {
-                            text += ' dag';
-                        } else {
-                            text += ' dagar';
-                        }
-
-                        return text;
-                    };
                 },
                 templateUrl: 'components/commonDirectives/rhsSlider/rhsSlider.directive.html'
             };
