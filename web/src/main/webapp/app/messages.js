5a2d89463ce4a2669e47e840abd4129adfc9a021
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-03-14 10:30:50 +0100

    REHAB-57: Uppdaterat texterna i klienten

--- web/src/main/webapp/app/messages.js
+++ web/src/main/webapp/app/messages.js
@@ -48,41 +48,65 @@ var rhsMessages = {
 
         // Sjukfall Start
         // Sjukfall Start Läkare
-        'label.sjukfall.start.lakare.header' : 'Dina sjukfall på enhet ',
-        'label.sjukfall.start.lakare.statheader' : 'Översikt - alla dina pågående sjukfall på enheten',
-        'label.sjukfall.start.lakare.selectionpanel.header' : 'Endast de sjukfall där jag utfärdat det aktiva intyget',
-        'label.sjukfall.start.lakare.selectionpanel.body' : 'Som läkare kan du ta del av de sjukskrivningar där du själv utfärdat det senaste intyget.<br><br>' +
-                                                    'När du går vidare och klickar på "Visa mina sjukfall" nedan kommer du att få se de pågående sjukfall där du har utfärdat det senaste intyget för den aktuella enheten. ' +
-                                                    'Om du sedan byter enhet kommer du få se dina pågående sjukfall för den enheten istället.',
+        'label.sjukfall.start.lakare.header' : 'Pågående sjukfall på ',
+        'label.sjukfall.start.lakare.selectionpanel.header' : 'Mina pågående sjukfall på ',
+        'label.sjukfall.start.lakare.selectionpanel.body' : 'Som läkare kan du ta del av de sjukfall där du själv utfärdat det nuvarande intyget.<br><br>' +
+                                                    'När du klickar på "Visa mina sjukfall" nedan kommer du att få se dina pågående sjukfall för den vårdenhet du har loggat in på. För varje sjukfall visas patientens personuppgifter, diagnos, sjukskrivningstid och sjukskrivningsgrad. ' +
+                                                    'Om du har tillgång till flera vårdenheter kan du se dina pågående sjukfall för en annan vårdenhet genom att byta vårdenhet i sidhuvudet. ' +
+                                                    '<br><br>Informationen som visas loggas enligt Patientdatalagen (PDL).',
         'label.sjukfall.start.lakare.selectionpanel.urval.button' : 'Visa mina sjukfall',
 
         // Sjukfall Start Rehab
-        'label.sjukfall.start.rehab.header' : 'Sjukfall på enhet ',
-        'label.sjukfall.start.rehab.statheader' : 'Översikt - alla pågående sjukfall på enheten',
-        'label.sjukfall.start.rehab.selectionpanel.header' : 'Alla pågående sjukfall på enheten',
-        'label.sjukfall.start.rehab.selectionpanel.body' : 'Som rehabkoordinator tar du del av alla pågående sjukskrivningar på enheten.<br><br>' +
-        'När du går vidare och klickar på "Visa alla sjukfall" nedan kommer du att få se alla pågående sjukfall för den aktuella enheten. ' +
-        'Om du sedan byter enhet kommer du få se alla pågående sjukfall för den enheten istället.',
+        'label.sjukfall.start.rehab.header' : 'Pågående sjukfall på ',
+        'label.sjukfall.start.rehab.selectionpanel.header' : 'Samtliga pågående sjukfall på ',
+        'label.sjukfall.start.rehab.selectionpanel.body' : 'Som rehabkoordinator kan du ta del av samtliga pågående sjukfall på vårdenheten.<br><br>' +
+                                                    'När du klickar på "Visa alla sjukfall" nedan kommer du att få se alla pågående sjukfall för den vårdenhet du har loggat in på. ' +
+                                                    'För varje sjukfall visas patientens personuppgifter, diagnos, sjukskrivningstid, sjukskrivningsgrad och läkare. ' +
+                                                    'Om du har tillgång till flera vårdenheter kan du se pågående sjukfall för en annan vårdenhet genom att byta vårdenhet i sidhuvudet.' +
+                                                    '<br><br>Informationen som visas loggas enligt Patientdatalagen (PDL).',
         'label.sjukfall.start.rehab.selectionpanel.urval.button' : 'Visa alla sjukfall',
 
 
 
         // Sjukfall Stat
-        'label.sjukfall.stat.ongoing.lakare' : 'Antal pågående sjukfall för dina patienter för enhet ',
-        'label.sjukfall.stat.gender.lakare' : 'Könsfördelning över dina patienter',
-        'label.sjukfall.stat.ongoing.rehab' : 'Antalet pågående sjukfall på',
+        'label.sjukfall.start.lakare.statheader' : 'Översikt - mina pågående sjukfall på ',
+        'label.sjukfall.start.rehab.statheader' : 'Översikt - alla pågående sjukfall på ',
+
+        'label.sjukfall.stat.ongoing.lakare' : 'Antal pågående sjukfall på enheten',
+        'label.sjukfall.stat.gender.lakare' : 'Könsfördelning',
+        'label.sjukfall.stat.ongoing.rehab' : 'Antal pågående sjukfall på enheten',
         'label.sjukfall.stat.gender.rehab' : 'Könsfördelning',
         'label.sjukfall.stat.male' : 'Män',
         'label.sjukfall.stat.female' : 'Kvinnor',
 
         // Sjukfall Result
-        'label.sjukfall.result.lakare.header': 'Lista på mina pågående sjukfall',
-        'label.sjukfall.result.lakare.subheader': ' - En lista över de sjukfall där du utfärdat det senaste intyget',
+        'label.sjukfall.result.lakare.header': 'Mina sjukfall',
+        'label.sjukfall.result.lakare.subheader': ' - Endast de pågående sjukfall där jag utfärdat det nuvarande intyget',
 
-        'label.sjukfall.result.rehab.header': 'Lista på alla pågående sjukfall',
-        'label.sjukfall.result.rehab.subheader': ' - En lista över alla enhetens pågående sjukfall',
+        'label.sjukfall.result.rehab.header': 'Alla sjukfall',
+        'label.sjukfall.result.rehab.subheader': ' - Samtliga pågående sjukfall på enheten ',
 
-        'label.sjukfall.result.back': 'Ändra urval',
+        'label.sjukfall.result.back': 'Tillbaka till översiktssidan',
+
+        // Filter
+        'label.filter.show' : 'Visa sökfilter',
+        'label.filter.hide' : 'Dölj sökfilter',
+        'label.filter.diagnos' : 'Välj diagnos',
+        'label.filter.lakare' : 'Välj läkare',
+        'label.filter.langd' : 'Välj sjukskrivningslängd',
+        'label.filter.filter' : 'Fritextfilter',
+        'label.filter.filter.placeholder' : 'Hitta sjukfall som innehåller...',
+        'label.filter.allselected' : 'Alla valda',
+
+        // Settings
+        'label.settings.header' : 'Antal dagar mellan intyg',
+        'label.settings.help' : 'Ställ in det antal dagar det maximalt får vara mellan två intyg för att de ska räknas som samma sjukfall.',
+        'label.settings.info' : 'Max antal dagar uppehåll mellan intyg är satt till:',
+
+        'label.settings.modal.body' : 'Välj hur många dagars uppehåll det maximalt får vara mellan två intyg för att de ska räknas som samma sjukfall.',
+        'label.settings.modal.label' : 'Välj max antal dagars uppehåll mellan intygen',
+
+
 
         //
         'label.gender.male': 'Man',
@@ -93,44 +117,29 @@ var rhsMessages = {
         'label.table.column.patient.id': 'Person&shy;nummer',
         'label.table.column.patient.namn': 'Namn',
         'label.table.column.patient.konshow': 'Kön',
-        'label.table.column.diagnos.intygsvarde': 'Nuvarande diagnos',
-        'label.table.column.diagnos.help': 'Huvuddiagnosen i det intyg som är giltigt just nu. För muspekaren över koden för att se vilken diagnos den motsvarar.',
+        'label.table.column.diagnos.intygsvarde': 'Diagnos',
+        'label.table.column.diagnos.help': 'Huvuddiagnos i nuvarande intyg. För muspekaren över koden för att se vilken diagnos den motsvarar.',
         'label.table.column.start': 'Startdatum',
-        'label.table.column.start.help': 'Datum då sjukskrivningen började på ${enhet}. Alla intyg för samma patient som följer på varandra med max ${glapp} dagars uppehåll räknas till samma sjukskrivning. Max antal dagars uppehåll mellan intyg kan ställas in i filter.',
+        'label.table.column.start.help': 'Datum då sjukfallet började på ${enhet}. Alla intyg för samma patient som följer på varandra med max ${glapp} dagars uppehåll räknas till samma sjukfall. Max antal dagars uppehåll mellan intyg kan ställas in i filtret.',
         'label.table.column.slut': 'Slutdatum',
-        'label.table.column.slut.help': 'Slutdatum för det intyg som är giltigt just nu, dvs. det datum då sjukskrivningen beräknas avslutas om den inte förlängs.',
+        'label.table.column.slut.help': 'Slutdatum för sjukfallet, dvs. den sista dagen då det finns ett giltigt intyg.',
         'label.table.column.aktivgrad': 'Sjukskrivnings&shy;grad',
-        'label.table.column.aktivgrad.help': 'Sjukskrivningsgraden i det intyg som är giltigt just nu. Om det innehåller flera grader anges de ordnade i tidsföljd med den just nu gällande graden fetstilsmarkerad.',
-        'label.table.column.lakare.namn': 'Nuvarande läkare',
-        'label.table.column.lakare.namn.help': 'Läkaren som utfärdat det intyg som är giltigt just nu.',
+        'label.table.column.aktivgrad.help': 'Sjukskrivningsgrad i nuvarande intyg. Om det innehåller flera grader anges de ordnade i tidsföljd med markering av den just nu gällande graden.',
+        'label.table.column.lakare.namn': 'Läkare',
+        'label.table.column.lakare.namn.help': 'Läkaren som utfärdat nuvarande intyg.',
         'label.table.column.dagar': 'Sjukskrivnings&shy;längd',
-        'label.table.column.dagar.help': 'Sjukskrivningens totala längd i dagar, från ”Startdatum vårdenhet” till ”Slutdatum”. Dagar då patienten inte haft något giltigt intyg räknas inte.',
+        'label.table.column.dagar.help': 'Sjukfallets totala längd i dagar, från startdatum till slutdatum. Eventuella dagar mellan intyg räknas inte in.',
+
+        'label.table.no-result': 'Det finns inga pågående sjukfall på ',
+        'label.table.no-filter-result': 'Inga sjukfall matchade filtreringen.',
 
         'label.table.number.of.rows' : 'Visar',
         'label.table.number.of.rows.of' : 'av',
         'label.table.column.sort.desc': 'Fallande',
         'label.table.column.sort.asc': 'Stigande',
 
-        'label.table.no-result': 'Det finns inga pågående sjukfall.',
-        'label.table.no-filter-result': 'Filtrering matchade inga sjukfall. Försök med någon annat.',
 
-        // Filter
-        'label.filter.diagnos' : 'Välj nuvarande diagnos',
-        'label.filter.lakare' : 'Välj nuvarande läkare',
-        'label.filter.langd' : 'Välj sjukskrivningslängd',
-        'label.filter.show' : 'Visa sökfilter',
-        'label.filter.hide' : 'Dölj sökfilter',
-        'label.filter.filter' : 'Fritextfilter',
-        'label.filter.filter.placeholder' : 'Hitta sjukfall som innehåller...',
-        'label.filter.allselected' : 'Alla valda',
 
-        // Settings
-        'label.settings.header' : 'Inställning',
-        'label.settings.help' : 'En hjälpande text...',
-        'label.settings.info' : 'Max antal dagar uppehåll mellan intyg är satt till:',
-
-        'label.settings.modal.body' : 'Sätt det intervall som du vill att maxantalet dagar mellan två intyg ska vara för att det ska räknas som ett sjukfall och inte två.',
-        'label.settings.modal.label' : 'Välj max antal dagars uppehåll mellan två intyg',
 
         // Export
         'label.export.button' : 'Exportera',
