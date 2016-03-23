d657ebdf1044ecd0f51bd51ce2c57ff8cf848c3f
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2016-03-23 15:05:27 +0100

    REHAB-173: Nytt test

--- test/dev/spec/sjukfallTable.spec.js
+++ test/dev/spec/sjukfallTable.spec.js
@@ -38,7 +38,7 @@ fdescribe('Hantera tabellen', function() {
     });
 
     it('Sortera på personnummer', function() {
-        element.all(by.css('th.column-patient-id')).last().click();
+        element.all(by.css('.rhs-table thead:not(.hidden) th.column-patient-id')).first().click();
 
         var columnPatientValue = sjukfallPage.tableBody.all(by.css('.column-patient-id')).first().getText();
         var columnNumberValue = sjukfallPage.tableBody.all(by.css('.column-number')).first().getText();
@@ -46,7 +46,7 @@ fdescribe('Hantera tabellen', function() {
         expect(columnPatientValue).toBe('19000207-9812 (116 år)');
         expect(columnNumberValue).toBe('1');
 
-        element.all(by.css('th.column-patient-id')).last().click();
+        element.all(by.css('.rhs-table thead:not(.hidden) th.column-patient-id')).click();
 
         columnPatientValue = sjukfallPage.tableBody.all(by.css('.column-patient-id')).first().getText();
         columnNumberValue = sjukfallPage.tableBody.all(by.css('.column-number')).first().getText();
