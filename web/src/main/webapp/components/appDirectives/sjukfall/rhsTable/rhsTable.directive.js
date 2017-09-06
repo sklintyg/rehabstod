8788ef71d06b298ded59e350d6477ada398ea82c
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2017-09-06 10:15:35 +0200

    INTYG-4410: container-fluid layout på sjukfallstabellen för att bättre utnyttja bredare skärmar

--- web/src/main/webapp/components/appDirectives/sjukfall/rhsTable/rhsTable.directive.js
+++ web/src/main/webapp/components/appDirectives/sjukfall/rhsTable/rhsTable.directive.js
@@ -25,7 +25,7 @@ angular.module('rehabstodApp')
             $scope.filter = SjukfallFilterViewState;
             $scope.model = SjukfallModel;
             $scope.user = UserModel.get();
-            $scope.noResultColspan = ($scope.user.urval !== 'ISSUED_BY_ME') ? 10 : 9;
+            $scope.noResultColspan = ($scope.user.urval !== 'ISSUED_BY_ME') ? 13 : 12;
 
             $scope.displayedCollection = [].concat($scope.model.get());
 
