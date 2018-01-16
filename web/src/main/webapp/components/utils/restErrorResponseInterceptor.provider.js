0cbd6806ae8ddbe23ed1ff3f6e8ee7f82384a07a
Magnus Ekstrand <magnus@ekstrand.net>
2018-01-16 11:50:24 +0100

    Revert "INTYG-5284: uppdatering av licens-plugin till att hantera även *.groovy och *.js"
    This reverts commit fc8dc93f6ea8c9c3f3e4246439a213548568132c.

--- web/src/main/webapp/components/utils/restErrorResponseInterceptor.provider.js
+++ web/src/main/webapp/components/utils/restErrorResponseInterceptor.provider.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ * Copyright (C) 2016 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 /**
  * Response intercepter catching all responses coming back through the $http
  * service. On 500 status responses it will check if the config contains message keys to display.
