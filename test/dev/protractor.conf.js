4e3fb0a5f7480e32f58d6ab3f47bc04a5d5a9218
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2018-06-20 15:59:56 +0200

    BUILDENV: Använder headless igen, tillåter osäkra certs

--- test/dev/protractor.conf.js
+++ test/dev/protractor.conf.js
@@ -39,65 +39,30 @@ var screenshotReporter = new HtmlScreenshotReporter({
 
 exports.config = {
     directConnect: true,
-    seleniumAddress: require('./../rehabstodTestTools/environment.js').envConfig.SELENIUM_ADDRESS,
+    //seleniumAddress: require('./../rehabstodTestTools/environment.js').envConfig.SELENIUM_ADDRESS,
     baseUrl: require('./../rehabstodTestTools/environment.js').envConfig.REHABSTOD_URL,
 
     specs: ['./spec/*.spec.js'],
 
     suites: {
-        //  testdata: './spec/generateTestData/**/*.spec.js',
-        //  clean: './spec/cleanTestData/**/*.spec.js',
         app: ['./spec/*.spec.js']
     },
 
-    // If chromeOnly is true, we dont need to start the selenium server. (seems we don't need to anyway? can this be removed?)
-    // If you want to test with firefox, then set this to false and change the browserName
-    //chromeOnly: false,
-
     // Capabilities to be passed to the webdriver instance. (ignored if multiCapabilities is used)
     capabilities: {
-
-        // IE11
-        /*browserName: 'internet explorer',
-        platform: 'ANY',
-        version: '11',*/
-
-        // Any other browser
         browserName: 'chrome', // possible values: phantomjs, firefox, chrome
-
-        // Run parallell instances of same browser (combine with any browser above)
-        shardTestFiles: false, // set to true to divide tests among instances
-        maxInstances: 1 // change to >1 for parallell instances
-        // chromeOptions: {
-        //     args: [ "--no-sandbox", "--headless", "--disable-gpu", "--window-size=1600x1024" ]
-        // }
+        acceptInsecureCerts: true,
+        chromeOptions: {
+             args: [ "--no-sandbox", "--headless", "--disable-gpu", "--window-size=1600x1024" ]
+        }
     },
 
-    // Run *different browsers* in parallell (optional, completely replaces 'capabilities' above)
-/*    multiCapabilities: [{
-        browserName: 'chrome',
-        shardTestFiles: true,
-        maxInstances: 1
-    }, {
-        browserName: 'firefox',
-        shardTestFiles: true,
-        maxInstances: 1
-    }],*/
     framework: 'jasmine',
     jasmineNodeOpts: {
         // If true, print colors to the terminal.
         showColors: true,
         // Default time to wait in ms before a test fails.
         defaultTimeoutInterval: 30000
-        // Function called to print jasmine results.
-        //print: function() {},
-        // If set, only execute specs whose names match the pattern, which is
-        // internally compiled to a RegExp.
-        //grep: 'pattern',
-        // Inverts 'grep' matches
-        //invertGrep: false
-        //isVerbose: true, // jasmine 1.3 only
-        //includeStackTrace: true // jasmine 1.3 only
     },
 
     // Setup the report before any tests start
