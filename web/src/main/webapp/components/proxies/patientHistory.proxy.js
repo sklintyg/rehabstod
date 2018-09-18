79a3d9ddc21e0e7cde7b531df45e4c456994b61b
Benny Samuelsson <benny@solidcore.se>
2018-09-18 13:22:12 +0200

    INTYG-5675: Tog bort gamla parametrar.

--- web/src/main/webapp/components/proxies/patientHistory.proxy.js
+++ web/src/main/webapp/components/proxies/patientHistory.proxy.js
@@ -18,7 +18,7 @@
  */
 
 angular.module('rehabstodApp').factory('patientHistoryProxy',
-    function($http, $log, $q, ObjectHelper, networkConfig, UserModel) {
+    function($http, $log, $q, ObjectHelper, networkConfig) {
     'use strict';
 
     /*
@@ -29,7 +29,6 @@ angular.module('rehabstodApp').factory('patientHistoryProxy',
         var promise = $q.defer();
 
         var query = {
-            maxIntygsGlapp: UserModel.get().preferences.maxAntalDagarMellanIntyg,
             patientId: patient.id
         };
 
