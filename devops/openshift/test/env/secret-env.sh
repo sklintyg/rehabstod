b5cb0e2b46eb8256f5e175cea1673458d46df56f
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-10-30 13:31:31 +0100

    BUILDENV: test environment updated

--- devops/openshift/test/env/secret-env.sh
+++ devops/openshift/test/env/secret-env.sh
@@ -3,16 +3,11 @@
 
 export DB_USERNAME=${DATABASE_USERNAME:-intyg}
 export DB_PASSWORD=${DATABASE_PASSWORD:-intyg}
-export DB_NAME=${DATABASE_NAME:-rehabstod_test}
-export DB_SERVER=$MYSQL_SERVICE_HOST
-export DB_PORT=$MYSQL_SERVICE_PORT
 
 export ACTIVEMQ_BROKER_USERNAME=${ACTIVEMQ_BROKER_USERNAME:-admin}
 export ACTIVEMQ_BROKER_PASSWORD=${ACTIVEMQ_BROKER_PASSWORD:-admin}
 
 export REDIS_PASSWORD=${REDIS_PASSWORD:-redis}
-export REDIS_PORT=$REDIS_SERVICE_PORT
-export REDIS_HOST=$REDIS_SERVICE_HOST
 
 # dev profile is default for pipeline
 SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE:-test,rhs-security-test,rhs-it-stub,wc-hsa-stub,wc-pu-stub,testability-api,caching-enabled,rhs-srs-stub}
@@ -22,9 +17,8 @@ export CATALINA_OPTS_APPEND="\
 -Dconfig.folder=/opt/$APP_NAME/config \
 -Dconfig.file=/opt/$APP_NAME/config/rehabstod.properties \
 -Dlogback.file=classpath:logback-ocp.xml \
--Dcertificate.folder=/opt/$APP_NAME/env \
+-Dcertificate.folder=/opt/$APP_NAME/certifikat \
 -Dcredentials.file=/opt/$APP_NAME/env/secret-env.properties \
 -Dresources.folder=/tmp/resources \
 -Dfile.encoding=UTF-8 \
--DbaseUrl=http://${APP_NAME}:8080 \
--Dcertificate.baseUrl=http://intygstjanst-${APP_NAME}:8080"
+-DbaseUrl=http://${APP_NAME}:8080"
