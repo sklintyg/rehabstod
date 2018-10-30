8193d5c79b8aab80bc5b1f22a7d62595c212d393
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-10-30 16:21:48 +0100

    BUILDENV: Profiles corrected

--- devops/openshift/test/env/secret-env.sh
+++ devops/openshift/test/env/secret-env.sh
@@ -10,7 +10,8 @@ export ACTIVEMQ_BROKER_PASSWORD=${ACTIVEMQ_BROKER_PASSWORD:-admin}
 export REDIS_PASSWORD=${REDIS_PASSWORD:-redis}
 
 # dev profile is default for pipeline
--SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE:-test,rhs-security-test,rhs-it-stub,wc-hsa-stub,wc-pu-stub,testability-api,caching-enabled,rhs-srs-stub,rhs-sparrtjanst-stub,rhs-samtyckestjanst-stub}
+SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE:-test,rhs-security-test,rhs-it-stub,wc-hsa-stub,wc-pu-stub,testability-api,caching-enabled,rhs-srs-stub,rhs-sparrtjanst-stub,rhs-samtyckestjanst-stub}
+
 export CATALINA_OPTS_APPEND="\
 -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE \
 -Dconfig.folder=/opt/$APP_NAME/config \
