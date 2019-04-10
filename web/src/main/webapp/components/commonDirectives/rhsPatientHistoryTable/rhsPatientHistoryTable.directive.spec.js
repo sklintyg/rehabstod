3fc87855031eaf9752889fe9fcbe5683bea8ddfd
Magnus Ekstrand <magnus@ekstrand.net>
2019-04-10 09:42:10 +0200

    INTYG-8405: Litet tillägg till js-test

--- web/src/main/webapp/components/commonDirectives/rhsPatientHistoryTable/rhsPatientHistoryTable.directive.spec.js
+++ web/src/main/webapp/components/commonDirectives/rhsPatientHistoryTable/rhsPatientHistoryTable.directive.spec.js
@@ -57,6 +57,8 @@ describe('Directive: rhsPatientHistoryTable', function() {
         }
 
         sjfMetaData.samtyckeFinns = false;
+        sjfMetaData.blockingServiceError = false;
+        sjfMetaData.consentServiceError = false;
 
         _patientHistoryViewState_.setSjfMetaData(sjfMetaData);
 
@@ -68,7 +70,6 @@ describe('Directive: rhsPatientHistoryTable', function() {
     }));
 
     it('should render correct number of sjukskrivningsgrader', function() {
-
         // Assert
         expect(elementScope.formatGrader([100])).toEqual('100%');
         expect(elementScope.formatGrader([100,25])).toEqual('100% &#10142; 25%');
@@ -78,7 +79,6 @@ describe('Directive: rhsPatientHistoryTable', function() {
     });
 
     it('should render correct number of sjukskrivningsgrader', function() {
-
         // Assert
         expect(elementScope.formatGrader([100])).toEqual('100%');
         expect(elementScope.formatGrader([100,25])).toEqual('100% &#10142; 25%');
