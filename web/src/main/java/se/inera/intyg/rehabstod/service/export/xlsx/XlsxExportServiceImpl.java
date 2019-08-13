a070e3a97efba80743b0949a9c97b8626eedec90
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2019-08-13 13:35:05 +0200

    INTYGFV-12007: Fixar cornercase med filter för 365+ / 365+ dagar sjukskrivningslängd (#93)

--- web/src/main/java/se/inera/intyg/rehabstod/service/export/xlsx/XlsxExportServiceImpl.java
+++ web/src/main/java/se/inera/intyg/rehabstod/service/export/xlsx/XlsxExportServiceImpl.java
@@ -22,7 +22,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
@@ -37,7 +36,6 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
-
 import se.inera.intyg.infra.monitoring.annotation.PrometheusTimeMethod;
 import se.inera.intyg.rehabstod.auth.RehabstodUser;
 import se.inera.intyg.rehabstod.auth.RehabstodUserPreferences.Preference;
@@ -45,6 +43,7 @@ import se.inera.intyg.rehabstod.common.util.YearMonthDateFormatter;
 import se.inera.intyg.rehabstod.service.Urval;
 import se.inera.intyg.rehabstod.service.export.BaseExportService;
 import se.inera.intyg.rehabstod.web.controller.api.dto.PrintSjukfallRequest;
+import se.inera.intyg.rehabstod.web.model.LangdIntervall;
 import se.inera.intyg.rehabstod.web.model.Patient;
 import se.inera.intyg.rehabstod.web.model.SjukfallEnhet;
 
@@ -61,6 +60,10 @@ public class XlsxExportServiceImpl extends BaseExportService implements XlsxExpo
     private static final int FILTER_HEADLINE_COLUMN = 1;
     private static final int FILTER_VALUE_COLUMN = 2;
     private static final int FILTER_END_COLUMN_SPAN = 6;
+
+    private static final String LANGD_OVER_ONE_YEAR = "366";
+    private static final String LANGD_OVER_ONE_YEAR_DISPLAYVALUE = "365+";
+    private static final String TEMPLATESTRING_SJUKSKRIVNINGSLANGD = "Mellan %s och %s dagar";
 
     private XSSFCellStyle boldStyle;
     private XSSFCellStyle filterMainHeaderStyle;
@@ -103,8 +106,7 @@ public class XlsxExportServiceImpl extends BaseExportService implements XlsxExpo
                 req.getAldersIntervall().getMin() + " - " + req.getAldersIntervall().getMax() + " år");
         rowNumber = addDiagnosKapitel(sheet, rowNumber++, FILTER_TITLE_VALDA_DIAGNOSER, req.getDiagnosGrupper()); // NOSONAR
         addFilterHeader(sheet, rowNumber++, FILTER_TITLE_VALD_SLUTDATUM, getFilterDate(req.getSlutdatumIntervall()));
-        addFilterHeader(sheet, rowNumber++, FILTER_TITLE_VALD_SJUKSKRIVNINGSLANGD,
-                req.getLangdIntervall().getMin() + " - " + req.getLangdIntervall().getMax() + " dagar");
+        addFilterHeader(sheet, rowNumber++, FILTER_TITLE_VALD_SJUKSKRIVNINGSLANGD, getLangdintervall(req.getLangdIntervall()));
         rowNumber = addLakareList(sheet, rowNumber++, FILTER_TITLE_VALDA_LAKARE, req.getLakare(), user); // NOSONAR
         rowNumber = addKompletteringsStatus(sheet, rowNumber++, FILTER_TITLE_KOMPLETTERINGSSTATUS, req.getKomplettering()); // NOSONAR
 
@@ -131,6 +133,16 @@ public class XlsxExportServiceImpl extends BaseExportService implements XlsxExpo
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         wb.write(baos);
         return baos.toByteArray();
+    }
+
+    private String getLangdintervall(LangdIntervall intervall) {
+        if (intervall == null) {
+            return "-";
+        }
+
+        String minDesc = LANGD_OVER_ONE_YEAR.equals(intervall.getMin()) ? LANGD_OVER_ONE_YEAR_DISPLAYVALUE : intervall.getMin();
+        String maxDesc = LANGD_OVER_ONE_YEAR.equals(intervall.getMax()) ? LANGD_OVER_ONE_YEAR_DISPLAYVALUE : intervall.getMax();
+        return String.format(TEMPLATESTRING_SJUKSKRIVNINGSLANGD, minDesc, maxDesc);
     }
 
     private int addKompletteringsStatus(XSSFSheet sheet, int currentRowNumber, String filterTitleKompletteringsstatus,
