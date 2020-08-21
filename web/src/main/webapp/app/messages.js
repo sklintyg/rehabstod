c2f13473039fd22f8564133a311e6f7c9ddbef7f
Mikael Heglind <mikael.heglind@soprasteria.com>
2020-08-21 13:44:47 +0200

    INTYGFV-13201: cleaning up code

--- web/src/main/webapp/app/messages.js
+++ web/src/main/webapp/app/messages.js
@@ -266,7 +266,6 @@ var rhsMessages = {
     'label.patienthistory.loading': 'Hämtar sjukfall för patient...',
     'label.patienthistory.empty': 'Tyvärr finns det inga giltiga intyg att visa i det här sjukfallet för tillfället. Det beror på att intygen har makulerats.',
 
-
     'label.extradiagnoser.sekretess': 'För patient med skyddade personuppgifter kan ingen ytterligare information hämtas från andra vårdenheter eller andra vårdgivare.',
     'label.extradiagnoser.none': 'Det finns för tillfället ingen ytterligare information att inhämta från andra vårdenheter eller andra vårdgivare.',
     'label.extradiagnoser.empty': 'Det finns för tillfället ingen information i denna kategori att inhämta.',
