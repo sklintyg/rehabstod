a1606dd0d4d8770a49adbbb9b9ed1034568c252f
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-04-12 15:43:33 +0200

    INTYGFV-15504: Added logging to get active sickleaves

--- web/src/main/java/se/inera/intyg/rehabstod/service/sjukfall/GetActiveSickLeavesServiceImpl.java
+++ web/src/main/java/se/inera/intyg/rehabstod/service/sjukfall/GetActiveSickLeavesServiceImpl.java
@@ -32,6 +32,7 @@ import se.inera.intyg.infra.sjukfall.dto.IntygParametrar;
 import se.inera.intyg.rehabstod.auth.RehabstodUser;
 import se.inera.intyg.rehabstod.integration.it.dto.SickLeavesRequestDTO;
 import se.inera.intyg.rehabstod.integration.it.service.IntygstjanstRestIntegrationService;
+import se.inera.intyg.rehabstod.logging.SickLeaveLogMessageFactory;
 import se.inera.intyg.rehabstod.service.diagnos.dto.DiagnosKapitel;
 import se.inera.intyg.rehabstod.service.diagnos.dto.DiagnosKategori;
 import se.inera.intyg.rehabstod.service.monitoring.MonitoringLogService;
@@ -75,11 +76,15 @@ public class GetActiveSickLeavesServiceImpl implements GetActiveSickLeavesServic
         final var request = getRequest(user, unitId, careUnitId, filterRequest);
 
         LOG.debug("Getting sick leaves for unit {}", careUnitId);
+        final var logFactory = new SickLeaveLogMessageFactory(System.currentTimeMillis());
         final var response = intygstjanstRestIntegrationService.getActiveSickLeaves(request);
+        LOG.debug(logFactory.message(SickLeaveLogMessageFactory.GET_ACTIVE_SICK_LEAVES, response.getContent().size()));
         final var convertedSickLeaves = convertSickLeaves(response.getContent(), certificateParameters);
 
         LOG.debug("Add patient names and filter on protected person for sick leaves");
+       logFactory.setStartTimer(System.currentTimeMillis());
         puService.enrichSjukfallWithPatientNamesAndFilterSekretess(convertedSickLeaves);
+        LOG.debug(logFactory.message(SickLeaveLogMessageFactory.ADD_PATIENT_INFORMATION, response.getContent().size()));
 
         LOG.debug("Logging that sick leaves have been fetched");
         performMonitorLogging(convertedSickLeaves, user.getHsaId(), unitId != null ? unitId : careUnitId);
