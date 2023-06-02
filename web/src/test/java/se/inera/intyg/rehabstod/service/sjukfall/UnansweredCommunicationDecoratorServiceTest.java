6032884396bf246ee83cddcf8324780711f6b2e2
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-06-02 14:30:49 +0200

    INTYGFV-15657: Added test for decoration

---
+++ web/src/test/java/se/inera/intyg/rehabstod/service/sjukfall/UnansweredCommunicationDecoratorServiceTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2023 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.rehabstod.service.sjukfall;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.inera.intyg.rehabstod.integration.wc.service.WcRestIntegrationService;
+import se.inera.intyg.rehabstod.integration.wc.service.dto.UnansweredCommunicationResponse;
+import se.inera.intyg.rehabstod.integration.wc.service.dto.UnansweredQAs;
+import se.inera.intyg.rehabstod.web.model.Patient;
+import se.inera.intyg.rehabstod.web.model.SjukfallEnhet;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UnansweredCommunicationDecoratorServiceTest {
+
+    @Mock
+    WcRestIntegrationService wcRestIntegrationService;
+
+    @InjectMocks
+    UnansweredCommunicationDecoratorServiceImpl unansweredCommunicationDecoratorService;
+
+    UnansweredCommunicationResponse response = new UnansweredCommunicationResponse();
+
+    private static final String CERTIFICATE_ID_0 = "C0";
+    private static final String CERTIFICATE_ID_1 = "C1";
+    private static final String CERTIFICATE_ID_2 = "C2";
+    private static final String CERTIFICATE_ID_3 = "C3";
+    private static final String PATIENT_ID_0 = "191212121212";
+    private static final String PATIENT_ID_1 = "201212121212";
+    private static final String PATIENT_ID_2 = "181212121212";
+    private static final SjukfallEnhet SICK_LEAVE_SEVERAL_CERTIFICATES = new SjukfallEnhet();
+    private static final SjukfallEnhet SICK_LEAVE_ONE_CERTIFICATE = new SjukfallEnhet();
+    private static final SjukfallEnhet SICK_LEAVE_NO_CERTIFICATE_WITH_COMMUNICATION = new SjukfallEnhet();
+    private static final List<SjukfallEnhet> SICK_LEAVES = List.of(
+            SICK_LEAVE_ONE_CERTIFICATE,
+            SICK_LEAVE_NO_CERTIFICATE_WITH_COMMUNICATION,
+            SICK_LEAVE_SEVERAL_CERTIFICATES
+    );
+    private static final UnansweredQAs QA_0 = new UnansweredQAs(0, 0);
+    private static final UnansweredQAs QA_1 = new UnansweredQAs(5, 3);
+    private static final UnansweredQAs QA_2 = new UnansweredQAs(2, 3);
+    private static final UnansweredQAs QA_3 = new UnansweredQAs(10, 20);
+
+    @BeforeEach
+    void setup() {
+        SICK_LEAVE_SEVERAL_CERTIFICATES.setIntygLista(Arrays.asList(CERTIFICATE_ID_0, CERTIFICATE_ID_1, CERTIFICATE_ID_2));
+        SICK_LEAVE_SEVERAL_CERTIFICATES.setPatient(new Patient(PATIENT_ID_0, "Name"));
+        SICK_LEAVE_ONE_CERTIFICATE.setIntygLista(List.of(CERTIFICATE_ID_3));
+        SICK_LEAVE_ONE_CERTIFICATE.setPatient(new Patient(PATIENT_ID_1, "Name"));
+        SICK_LEAVE_NO_CERTIFICATE_WITH_COMMUNICATION.setIntygLista(List.of("NO_COMMUNICATION"));
+        SICK_LEAVE_NO_CERTIFICATE_WITH_COMMUNICATION.setPatient(new Patient(PATIENT_ID_2, "Name"));
+
+        final var unansweredQAsMap = new HashMap<String, UnansweredQAs>();
+        unansweredQAsMap.put(CERTIFICATE_ID_0, QA_0);
+        unansweredQAsMap.put(CERTIFICATE_ID_1, QA_1);
+        unansweredQAsMap.put(CERTIFICATE_ID_2, QA_2);
+        unansweredQAsMap.put(CERTIFICATE_ID_3, QA_3);
+        response.setUnansweredQAsMap(unansweredQAsMap);
+
+        when(wcRestIntegrationService.getUnansweredCommunicationForPatients(anyList())).thenReturn(response);
+    }
+
+    @Nested
+    class TestItRequest {
+
+        @Test
+        void shouldCallRestServiceWithPatientIds() {
+            final var captor = ArgumentCaptor.forClass(List.class);
+
+            unansweredCommunicationDecoratorService.decorate(SICK_LEAVES);
+
+            verify(wcRestIntegrationService).getUnansweredCommunicationForPatients(captor.capture());
+            assertEquals(SICK_LEAVES.size(), captor.getValue().size());
+            assertTrue(captor.getValue().contains(PATIENT_ID_0));
+            assertTrue(captor.getValue().contains(PATIENT_ID_1));
+            assertTrue(captor.getValue().contains(PATIENT_ID_2));
+        }
+    }
+
+    @Nested
+    class TestResponse {
+        @Test
+        void shouldSetErrorAsTrueIfResponseIsSuccess() {
+            response.setUnansweredCommunicationError(true);
+
+            final var hasDecorated = unansweredCommunicationDecoratorService.decorate(SICK_LEAVES);
+
+            assertFalse(hasDecorated);
+        }
+
+        @Test
+        void shouldSetErrorAsFalseIfResponseIsFail() {
+            response.setUnansweredCommunicationError(false);
+
+            final var hasDecorated = unansweredCommunicationDecoratorService.decorate(SICK_LEAVES);
+
+            assertTrue(hasDecorated);
+        }
+    }
+
+    @Nested
+    class TestDecoration {
+        @Test
+        void shouldNotDecorateCertificateWithNoCommunication() {
+            unansweredCommunicationDecoratorService.decorate(SICK_LEAVES);
+
+            assertEquals(0, SICK_LEAVE_NO_CERTIFICATE_WITH_COMMUNICATION.getUnansweredOther());
+            assertEquals(0, SICK_LEAVE_NO_CERTIFICATE_WITH_COMMUNICATION.getObesvaradeKompl());
+        }
+
+        @Test
+        void shouldSetValueOfSickLeaveWithOneCertificateWithCommunication() {
+            unansweredCommunicationDecoratorService.decorate(SICK_LEAVES);
+
+            assertEquals(20, SICK_LEAVE_ONE_CERTIFICATE.getUnansweredOther());
+            assertEquals(10, SICK_LEAVE_ONE_CERTIFICATE.getObesvaradeKompl());
+        }
+
+        @Test
+        void shouldAddValuesOfSickLeaveWithSeveralCertificatesWithCommunication() {
+            unansweredCommunicationDecoratorService.decorate(SICK_LEAVES);
+
+            assertEquals(6, SICK_LEAVE_SEVERAL_CERTIFICATES.getUnansweredOther());
+            assertEquals(7, SICK_LEAVE_SEVERAL_CERTIFICATES.getObesvaradeKompl());
+        }
+    }
+}
