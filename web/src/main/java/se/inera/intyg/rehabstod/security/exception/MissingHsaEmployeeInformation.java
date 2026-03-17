ad4a3b625b416fd0f10561ebbd07772eedfd414c
Christoffer Sigerhed <christoffer.sigerhed@gmail.com>
2026-03-17 14:38:44 +0100

    K1J-1526: Spotless fix. (#480)

--- web/src/main/java/se/inera/intyg/rehabstod/security/exception/MissingHsaEmployeeInformation.java
+++ web/src/main/java/se/inera/intyg/rehabstod/security/exception/MissingHsaEmployeeInformation.java
@@ -20,9 +20,7 @@ package se.inera.intyg.rehabstod.security.exception;
 
 import org.springframework.security.core.AuthenticationException;
 
-/**
- * Created by eriklupander on 2016-05-11.
- */
+/** Created by eriklupander on 2016-05-11. */
 public class MissingHsaEmployeeInformation extends AuthenticationException {
 
   public MissingHsaEmployeeInformation(String msg) {
