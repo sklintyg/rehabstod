2026cc9ba9b506a3e056397681d6da08bad735d9
cigerhed <christoffer.sigerhed@soprasteria.com>
2026-03-20 15:26:22 +0100

    K1J-1526: Update HSA cache keys.

--- integration/hsa-integration-intyg-proxy-service/src/main/java/se/inera/intyg/rehabstod/integration/intygproxyservice/constants/HsaIntygProxyServiceConstants.java
+++ integration/hsa-integration-intyg-proxy-service/src/main/java/se/inera/intyg/rehabstod/integration/intygproxyservice/constants/HsaIntygProxyServiceConstants.java
@@ -24,12 +24,12 @@ public class HsaIntygProxyServiceConstants {
     throw new IllegalStateException("Utility class!");
   }
 
-  public static final String EMPLOYEE_CACHE_NAME = "hsaIntygProxyServiceEmployeeCache";
+  public static final String EMPLOYEE_CACHE_NAME = "rehabStodHsaIntygProxyServiceEmployeeCache";
   public static final String HEALTH_CARE_UNIT_CACHE_NAME =
-      "hsaIntygProxyServiceHealthCareUnitCache";
+      "rehabStodHsaIntygProxyServiceHealthCareUnitCache";
   public static final String HEALTH_CARE_UNIT_MEMBERS_CACHE_NAME =
-      "hsaIntygProxyServiceHealthCareUnitMembersCache";
+      "rehabStodHsaIntygProxyServiceHealthCareUnitMembersCache";
   public static final String UNIT_CACHE_NAME = "hsaIntygProxyServiceUnitCache";
   public static final String HEALTH_CARE_PROVIDER_CACHE_NAME =
-      "hsaIntygProxyServiceHealthCareProviderCache";
+      "rehabStodHsaIntygProxyServiceHealthCareProviderCache";
 }
